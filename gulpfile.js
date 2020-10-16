c871857f87b37892852dcb7f95cc59e58ff055e5
sunshine <sunshine.li@1data.info>
2020-10-16 17:07:26 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,132 @@
+// 实现这个项目的构建任务
+const { src, dest, parallel, series, watch } = require('gulp')
+
+const del = require('del')
+const browserSync = require('browser-sync')
+
+const loadPlugins = require('gulp-load-plugins')
+
+const plugins = loadPlugins()
+const bs = browserSync.create()
+
+const data = {
+  menus: [
+    {
+      name: 'Home',
+      icon: 'aperture',
+      link: 'index.html'
+    },
+    {
+      name: 'About',
+      link: 'about.html'
+    }
+  ],
+  pkg: require('./package.json'),
+  date: new Date()
+}
+
+// 情况输出目录文件
+const clean = () => {
+  return del(['dist', 'temp'])
+}
+
+// 编译css样式
+const style = () => {
+  return src('src/assets/styles/*.scss', { base: 'src' })
+    .pipe(plugins.sass({ outputStyle: 'expanded' })) 
+    .pipe(dest('temp')) // dest目标位置
+    .pipe(bs.reload({ stream: true }))
+}
+
+// 编译js脚本
+const script = () => {
+  return src('src/assets/scripts/*.js', { base: 'src' })
+    .pipe(plugins.babel({ presets: ['@babel/preset-env'] }))
+    .pipe(dest('temp'))
+    .pipe(bs.reload({ stream: true }))
+}
+
+// 编译html页面
+const page = () => {
+  return src('src/*.html', { base: 'src' })
+    .pipe(plugins.swig({ data, defaults: { cache: false } })) // 防止模板缓存导致页面不能及时更新
+    .pipe(dest('temp'))
+    .pipe(bs.reload({ stream: true }))
+}
+
+// 转换图片
+const image = () => {
+  return src('src/assets/images/**', { base: 'src' })
+    .pipe(plugins.imagemin())
+    .pipe(dest('dist'))
+}
+
+// 转换字体
+const font = () => {
+  return src('src/assets/fonts/**', { base: 'src' })
+    .pipe(plugins.imagemin())
+    .pipe(dest('dist'))
+}
+
+// 其它额外的文件拷贝
+const extra = () => {
+  return src('public/**', { base: 'public' })
+    .pipe(dest('dist'))
+}
+
+const server = () => {
+  watch('src/assets/styles/*.scss', style)
+  watch('src/assets/scripts/*.js', script)
+  watch('src/*.html', page)
+  watch([
+    'src/assets/images/**',
+    'src/assets/fonts/**',
+    'public/**'
+  ], bs.reload)
+
+  bs.init({
+    notify: false,
+    port: 2080,
+    server: {
+      baseDir: ['temp', 'src', 'public'],
+      routes: {
+        '/node_modules': 'node_modules'
+      }
+    }
+  })
+}
+
+const useref = () => {
+  return src('temp/*.html', { base: 'temp' })
+    .pipe(plugins.useref({ searchPath: ['temp', '.'] }))
+    // html js css
+    .pipe(plugins.if(/\.js$/, plugins.uglify()))
+    .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
+    .pipe(plugins.if(/\.html$/, plugins.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    })))
+    .pipe(dest('dist'))
+}
+
+// 合并任务，并行处理
+const compile = parallel(style, script, page)
+
+// 上线之前执行的任务
+const build =  series(
+  clean,
+  parallel(
+    series(compile, useref),
+    image,
+    font,
+    extra
+  )
+)
+const develop = series(compile, server)
+module.exports = {
+  clean,
+  build,
+  serve: develop,
+  start: develop
+}
